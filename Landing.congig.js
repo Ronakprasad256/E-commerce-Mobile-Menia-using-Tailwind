0793dd38530f0d8da5b1bab4d414465ddceb8ea3
Ronak <ronakpd07@gmail.com>
2023-10-12 18:51:46 +0530

    E-commerce-Tailwindcss

---
+++ Landing.congig.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     theme: {
+//       screens: {
+//         'sm': '320px',
+//         // => @media (min-width: 640px) { ... }
+  
+//         'md': '450px',
+//         // => @media (min-width: 768px) { ... }
+  
+//         'lg': '645px',
+//         // => @media (min-width: 1024px) { ... }
+  
+//         'xl': '930px',
+//         // => @media (min-width: 1280px) { ... }
+  
+//         '2xl': '1536px',
+//         // => @media (min-width: 1536px) { ... }
+//       }
+//     }
+//   }
